b294c0445ca0ea907ae8bbb3cdc1f908aa0c2041
29244 <29244@ma-web.nl>
2020-06-07 21:25:57 +0200

    almost complete

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
